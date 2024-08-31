7a13cfec415fc50b0de1b0e35f3557916a060016
riteshivankar <riteshshivankar8@gmail.com>
2024-08-31 20:29:27 +0530

    2nd last commit

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -10,32 +10,36 @@ const App = () => {
   const ranX = gsap.utils.random(-500, 500, 50);
   const ranY = gsap.utils.random(-500, 500, 100);
   const rota = gsap.utils.random(0, 360, 30);
-  const [xVal, setxVal] = useState(0);
-  const [yVal, setyVal] = useState(0);
-  const [rot, setrot] = useState(0);
+  // const [xVal, setxVal] = useState(0);
+  // const [yVal, setyVal] = useState(0);
+  // const [rot, setrot] = useState(0);
   var imageRef = useRef();
-  useGSAP(() => {
+  const funcky = function(){
+
     gsap.to(
       imageRef.current,
       {
-        x:xVal,
-        y:yVal,
-        rotate:rot,
-        duration:0.6,
+        x:ranX,
+        y:ranY,
+        rotate:rota,
+        duration:2,
         delay:1
       },
-      {scope:'main',dependencies:[xVal,yVal,rot]}
+      {scope:'main',dependencies:[ranX,ranY,rota]}
     );
+  }
+  useGSAP(() => {
   });
   return (
     <main>
+      <button onClick={funcky}>Animate</button>
       <img
-        onClick={() => {
-          setxVal(ranX);
-          setyVal(ranY);
-          setrot(rota);
-          console.log(ranX, ranY, rota);
-        }}
+        // onClick={() => {
+        //   setxVal(ranX);
+        //   setyVal(ranY);
+        //   setrot(rota);
+        //   console.log(ranX, ranY, rota);
+        // }}
         ref={imageRef}
         src="https://e7.pngegg.com/pngimages/293/497/png-clipart-beetle-silhouette-beetle-animals-silhouette-thumbnail.png"
         alt=""
